54ad66ee537f7a82e1e5ffe358ae722b7774d844
coincity <coincity.games@outlook.com>
2024-11-22 21:36:53 -0600

    Se modifica el footer

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,17 +1,18 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <footer className="footer-section">
         <div className="footer-container">
           <div className="footer-block">
-            <span>Acerca de Crédito PyME México</span>
-            <p>Crédito PyME México gestiona y promociona financiamiento para empresas y personas en todo el territorio nacional y se ajusta a los requisitos y condiciones de cada Banco y/o Financiera con la que se hace la gestión, en cuyos sitios podrá ver mayor detalle de la información aquí presentada.</p>
-            <p>La información mostrada en este sitio web es unicamente de carácter informativo y no representa obligación alguna por parte de Crédito PyME México o Financieras o Bancos con los que se realiza el trámite.</p>
+            <span>Acerca de Coincity</span>
+            <p>En CoinCity, transformamos la forma de hacer negocios a través de soluciones innovadoras en máquinas vending. Nos especializamos en la compra, venta y renta de equipos de alta calidad, diseñados para satisfacer las necesidades de emprendedores, empresas y eventos en busca de automatización y rentabilidad.</p>
+            <p>Con un enfoque en la tecnología y el servicio al cliente, en CoinCity ofrecemos más que máquinas; brindamos herramientas para que nuestros clientes expandan sus oportunidades de negocio. Nos enorgullece trabajar de la mano contigo para personalizar soluciones que impulsen tu éxito.</p>
+            <p>Ya sea que busques iniciar un negocio vending, optimizar tus espacios comerciales o ofrecer servicios únicos en tus eventos, en CoinCity encontrarás la experiencia y los productos ideales para llevar tu visión al siguiente nivel.</p>
           </div>
           <div className="footer-block">
             <span>AVISOS</span>
-            <span className="footer-links">
+            {/* <span className="footer-links">
               <div className="shield-icon icon"></div>
               &nbsp;
               <Link 
@@ -30,15 +31,15 @@ export const Footer = () => {
               >
                 POLÍTICA DE COOKIES
               </Link>
-            </span>
+            </span> */}
           </div>
           <div className="footer-block">
-            <span>SOCIAL</span>
+            {/* <span>SOCIAL</span>
             <span className="footer-links">
               <div className="twitter-icon icon"></div>
               &nbsp;
               <a href="https://twitter.com/credito_pyme_mx" target="_blank" rel="noreferrer noopener" title="Regálanos un RT 😊">TWITTER</a>
-            </span>
+            </span> */}
           </div>
         </div>
     </footer>

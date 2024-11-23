a60ba426a523b1d1a7d631e7939149382260c206
coincity <coincity.games@outlook.com>
2024-11-22 20:16:00 -0600

    Se midifica el main section

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import { CoincityApp } from "../CoincityApp";
-import { CookiesPage, PrivacyPage, NotFound } from "../ui";
+import { NotFound } from "../ui";
 import { useEffect } from "react";
 
 export const AppRouter = () => {
@@ -15,8 +15,6 @@ export const AppRouter = () => {
   return (
     <Routes>
         <Route path="/" element={ <CoincityApp /> } />
-        <Route path="/politica-de-privacidad" element={ <PrivacyPage /> } />
-        <Route path="/politica-de-cookies" element={ <CookiesPage /> } />
         <Route path="/*" element={ <NotFound /> } />
     </Routes>
   );
--- src/ui/sections/HeroeSection.tsx
+++ src/ui/sections/HeroeSection.tsx
@@ -3,9 +3,9 @@ import { HeroeSlide } from "../components/HeroeSlide";
 export const HeroeSection = () => {
   return (
     <section className="heroe-section">
-        <h1 className="heroe-title">Crédito PyME México</h1>
-        <h2 className="heroe-subtitle">Financiamiento a la medida de tu Empresa</h2>
-        <p className="heroe-paragraph">Encontramos el financiamiento idóneo a tu perfil comercial, fiscal y crediticio, accede a el capital necesario para tu negocio o emprendimiento</p>
+        <h1 className="heroe-title">Coincity</h1>
+        <h2 className="heroe-subtitle">Vending Machines</h2>
+        <p className="heroe-paragraph">Compra, venta y renta de máquinas vending de última generación</p>
         <a 
           href="#mas-informacion"
           className="button-main-color"
